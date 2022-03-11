6a4e48b53f99d81443a1249d911aa4522f04b546
Bonnie <76897264+zmffhqj714@users.noreply.github.com>
2022-03-11 21:07:48 +0900

    Update AjaxController.java

--- src/main/java/com/somebody/controller/AjaxController.java
+++ src/main/java/com/somebody/controller/AjaxController.java
@@ -1,247 +1,215 @@
 package com.somebody.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+	@@ -41,103 +43,103 @@ public AjaxController(){}
 
-
-import com.somebody.serviece.auth.Authenticaion;
-import com.somebody.serviece.equimpment.Equipment;
-import com.somebody.serviece.lesson.Lesson;
-import com.somebody.serviece.member.Member;
-import com.somebody.serviece.pay.Pay;
-import com.somebody.serviece.staff.Staff;
-
-import beans.Centers;
-import beans.Equipments;
-import beans.Lessons;
-import beans.Members;
-import beans.Pays;
-
-
-@RestController
-@RequestMapping(value ="/ajax",produces = "application/json;charset=UTF-8")
-public class AjaxController {
-	@Autowired
-	Authenticaion auth;
-	@Autowired
-	Member me;
-	@Autowired
-	Staff sf;
-	@Autowired
-	Lesson ls;
-	@Autowired
-	Equipment eq;
-	@Autowired
-	Pay pa;
-	
-	public AjaxController(){}
 	//Bon
 	@RequestMapping(value = "/modPw", method = RequestMethod.POST)
-	public void modPw(Model model, @ModelAttribute Centers ct) {
+	public void modPw(Model model, @RequestBody Centers ct) {
 		this.auth.backController("A06", ct);
 	}
 	@RequestMapping(value = "/meMg", method = RequestMethod.POST)
-	public void meMg(Model model, @ModelAttribute Members me) {
+	public void meMg(Model model, @RequestBody Members me) {
 		this.me.backController("M01",me);
 	}
 
 	@RequestMapping(value = "/searchMeMg", method = RequestMethod.POST)
-	public void searchMeMg(Model model, @ModelAttribute Members me) {
+	public void searchMeMg(Model model, @RequestBody Members me) {
 		this.me.backController("M02",me);
 	}
 
 	@RequestMapping(value = "/meDetail", method = RequestMethod.POST)
-	public void meDetail(Model model, @ModelAttribute Members me) {
+	public void meDetail(Model model, @RequestBody Members me) {
 		this.me.backController("M03",me);
 	}
 
 	@RequestMapping(value = "/getCaList", method = RequestMethod.POST)
-	public void getCaList(Model model, @ModelAttribute Members me) {
+	public void getCaList(Model model, @RequestBody Members me) {
 		this.me.backController("M04", me);
 	}
 
 	@RequestMapping(value = "/addMember", method = RequestMethod.POST)
-	public void addMember(Model model, @ModelAttribute Members me) {
+	public void addMember(Model model, @RequestBody Members me) {
 		this.me.backController("M06",me);
-	}	
+	}
 
 	@RequestMapping(value = "/modMe", method = RequestMethod.POST)
-	public void modMe(Model model, @ModelAttribute Members me) {
+	public void modMe(Model model, @RequestBody Members me) {
 		this.me.backController("M07", me);
-		
+
 	}
 
 	@RequestMapping(value = "/insInbody", method = RequestMethod.POST)
-	public void insInbody(Model model, @ModelAttribute Members me) {
+	public void insInbody(Model model, @RequestBody Members me) {
 		this.me.backController("M09", me);
 	}
-	
-	
-	
+
+
+
 	//Dong
 	@RequestMapping(value = "/sfMg", method = RequestMethod.POST)
-	public void sfMg(Model model, @ModelAttribute Staff sf) {
+	public void sfMg(Model model, @RequestBody Staff sf) {
 		this.sf.backController("S01", sf);
 	}
 	@RequestMapping(value = "/searchSfMg", method = RequestMethod.POST)
-	public void searchSfMg(Model model, @ModelAttribute Staff sf) {
+	public void searchSfMg(Model model, @RequestBody Staff sf) {
 		this.sf.backController("S02", sf);
 	}
 	@RequestMapping(value = "/getMaxSf", method = RequestMethod.POST)
-	public void getMaxSf(Model model, @ModelAttribute Staff sf) {
+	public void getMaxSf(Model model, @RequestBody Staff sf) {
 		this.sf.backController("S03", sf);
 	}
 	@RequestMapping(value = "/insSf", method = RequestMethod.POST)
-	public void insSf(Model model, @ModelAttribute Staff sf) {
+	public void insSf(Model model, @RequestBody Staff sf) {
 		this.sf.backController("S04", sf);
 	}
 	@RequestMapping(value = "/modSf", method = RequestMethod.POST)
-	public void modSf(Model model, @ModelAttribute Staff sf) {
+	public void modSf(Model model, @RequestBody Staff sf) {
 		this.sf.backController("S07", sf);
 	}
 	@RequestMapping(value = "/getMeMg", method = RequestMethod.POST)
-	public void getMeMg(Model model, @ModelAttribute Staff sf) {
+	public void getMeMg(Model model, @RequestBody Staff sf) {
 		this.sf.backController("S09", sf);
 	}
-	
+
 	@RequestMapping(value = "/lessonMg", method = RequestMethod.POST)
-	public void lessonMg(Model model, @ModelAttribute Lessons ls) {
+	public void lessonMg(Model model, @RequestBody Lesson ls) {
 		this.ls.backController("L02",ls);
 	}
 	@RequestMapping(value = "/searchLesson", method = RequestMethod.POST)
-	public void searchLesson(Model model, @ModelAttribute Lessons ls) {
+	public void searchLesson(Model model, @RequestBody Lesson ls) {
 		this.ls.backController("L03",ls);
 	}
 	@RequestMapping(value = "/getLsCaList", method = RequestMethod.POST)
-	public void getLsCaList(Model model, @ModelAttribute Lessons ls) {
+	public void getLsCaList(Model model, @RequestBody Lesson ls) {
 		this.ls.backController("L04",ls);
 	}
 	@RequestMapping(value = "/insLsPay", method = RequestMethod.POST)
-	public void insLsPay(Model model, @ModelAttribute Lessons ls) {
+	public void insLsPay(Model model, @RequestBody Lesson ls) {
 		this.ls.backController("L05",ls);
 	}
 	@RequestMapping(value = "/getMaxLesson", method = RequestMethod.POST)
-	public void getMaxLesson(Model model, @ModelAttribute Lessons ls) {
+	public void getMaxLesson(Model model, @RequestBody Lesson ls) {
 		this.ls.backController("L06",ls);
 	}
 	@RequestMapping(value = "/insLesson", method = RequestMethod.POST)
-	public void insLesson(Model model, @ModelAttribute Lessons ls) {
+	public void insLesson(Model model, @RequestBody Lesson ls) {
 		this.ls.backController("L07",ls);
 	}
-	
-	
-	
+
+
+
 	//Young
 	@RequestMapping(value = "/modLesson", method = RequestMethod.POST)
-	public void modLesson(Model model, @ModelAttribute Lessons ls) {
+	public void modLesson(Model model, @RequestBody Lesson ls) {
 		this.ls.backController("L09",ls);
 	}
 	@RequestMapping(value = "/lsMemDetail", method = RequestMethod.POST)
-	public void lsMemDetail(Model model, @ModelAttribute Lessons ls) {
-		this.ls.backController("L10",ls);
+	@@ -146,99 +148,101 @@ public void lsMemDetail() {
 	}
-	
+
 	@RequestMapping(value = "/modLsSuccess", method = RequestMethod.POST)
-	public void modLsSuccess(Model model, @ModelAttribute Lessons ls) {
+	public void modLsSuccess(Model model, @RequestBody Lesson ls) {
 		this.ls.backController("L11",ls);
 	}
 	@RequestMapping(value = "/delLesson", method = RequestMethod.POST)
-	public void delLesson(Model model, @ModelAttribute Lessons ls) {
+	public void delLesson(Model model, @RequestBody Lesson ls) {
 		this.ls.backController("L12",ls);
 	}
 	@RequestMapping(value = "/goodsMg", method = RequestMethod.POST)
-	public void goodsMg(Model model, @ModelAttribute Equipments eq) {
-		this.eq.backController("G02",eq);
+	public List<Equipments> goodsMg(Model model, @RequestBody Equipments[] eq) {
+		this.eq.backController("G02",eq[0], model);
+		return (List<Equipments>) model.getAttribute("goodsList");
 	}
-	
+
 	@RequestMapping(value = "/searchGoods", method = RequestMethod.POST)
-	public void searchGoods(Model model, @ModelAttribute Equipments eq) {
-		this.eq.backController("G03",eq);
+	public List<Equipments> searchGoods(Model model, @RequestBody Equipments[] eq) {
+		this.eq.backController("G03",eq[0], model);
+		return (List<Equipments>) model.getAttribute("searchGoods");
 	}
-	
+
 	@RequestMapping(value = "/getGoodsCode", method = RequestMethod.POST)
-	public void getGoodsCode(Model model, @ModelAttribute Equipments eq) {
-		this.eq.backController("G04", eq);
+	public void getGoodsCode(Model model, @RequestBody Equipments[] eq) {
+		this.eq.backController("G04", eq[0], model);
 	}
 	@RequestMapping(value = "/insGoods", method = RequestMethod.POST)
-	public void insGoods(Model model, @ModelAttribute Equipments eq) {
-		this.eq.backController("G05", eq);
+	public void insGoods(Model model, @RequestBody Equipments[] eq) {
+		this.eq.backController("G05", eq[0], model);
 	}
 	@RequestMapping(value = "/modGoods", method = RequestMethod.POST)
-	public void modGoods(Model model, @ModelAttribute Equipments eq) {
-		this.eq.backController("G07", eq);
+	public void modGoods(Model model, @RequestBody Equipments[] eq) {
+		this.eq.backController("G07", eq[0], model);
 	}
 
 	@RequestMapping(value = "/onLoadPay", method = RequestMethod.POST)
-	public void onLoadPay(Model model, @ModelAttribute Pays pa) {
+	public void onLoadPay(Model model, @RequestBody Pays pa) {
 		this.pa.backController("P02", pa);
 	}
 
 	@RequestMapping(value = "/searchPay", method = RequestMethod.POST)
-	public void searchPay(Model model, @ModelAttribute Pays pa) {
+	public void searchPay(Model model, @RequestBody Pays pa) {
 		this.pa.backController("P03", pa);
 	}
 	@RequestMapping(value = "/goMeJoinPage", method = RequestMethod.POST)
-	public void goMeJoinPage(Model model,@RequestBody Members[] me) {
-		this.auth.backController2("J03", model.addAttribute("sendbean",me[0]));
+	public void goMeJoinPage(Model model, @RequestBody Members me) {
+		this.auth.backController2("J03", me);
 	}
-	
-	
-	
+
+
+
 	//UONE
 	@RequestMapping(value = "/infoLine", method = RequestMethod.POST)
-	public void infoLine(Model model, @ModelAttribute Members me) {
+	public void infoLine(Model model, @RequestBody Members me) {
 		this.me.backController("C01",me);
 	}
 	@RequestMapping(value = "/meDtInfo", method = RequestMethod.POST)
-	public void meDtInfo(Model model, @ModelAttribute Members me) {
+	public void meDtInfo(Model model, @RequestBody Members me) {
 		this.me.backController("C02",me);
 	}
 	@RequestMapping(value = "/meInbodyMg", method = RequestMethod.POST)
-	public void meInbodyMg(Model model, @ModelAttribute Members me) {
+	public void meInbodyMg(Model model, @RequestBody Members me) {
 		this.me.backController("C03", me);
 	}
 	@RequestMapping(value = "/insTaState", method = RequestMethod.POST)
-	public void insTaState(Model model, @ModelAttribute Members me) {
+	public void insTaState(Model model, @RequestBody Members me) {
 		this.me.backController("C04",me);
 	}
 	@RequestMapping(value = "/meHealthMg", method = RequestMethod.POST)
-	public void meHealthMg(Model model, @ModelAttribute Members me) {
+	public void meHealthMg(Model model, @RequestBody Members me) {
 		this.me.backController("C05",me);
 	}
 	@RequestMapping(value = "/meFoodMg", method = RequestMethod.POST)
-	public void meFoodMg(Model model, @ModelAttribute Members me) {
+	public void meFoodMg(Model model, @RequestBody Members me) {
 		this.me.backController("C06",me);
 	}
 	@RequestMapping(value = "/getLessonList", method = RequestMethod.POST)
-	public void getLessonList(Model model, @ModelAttribute Members me) {
+	public void getLessonList(Model model, @RequestBody Members me) {
 		this.me.backController("C08",me);
 	}
 	@RequestMapping(value = "/searchLsMg", method = RequestMethod.POST)
-	public void searchLsMg(Model model, @ModelAttribute Members me) {
+	public void searchLsMg(Model model, @RequestBody Members me) {
 		this.me.backController("C09",me);
 	}
 	@RequestMapping(value = "/insMeLesson", method = RequestMethod.POST)
-	public void insMeLesson(Model model, @ModelAttribute Members me) {
+	public void insMeLesson(Model model, @RequestBody Members me) {
 		this.me.backController("C10",me);
 	}
 	@RequestMapping(value = "/delMeLesson", method = RequestMethod.POST)
-	public void delMeLesson(Model model, @ModelAttribute Members me) {
+	public void delMeLesson(Model model, @RequestBody Members me) {
 		this.me.backController("C11",me);
 	}
 	@RequestMapping(value = "/checkMePw", method = RequestMethod.POST)
-	public void checkMePw(Model model, @RequestBody Members[] me) {
-		this.auth.backController2("C14",model.addAttribute("sendbean",me[0]));
+	public void checkMePw(Model model, @RequestBody Members me) {
+		this.auth.backController2("C14",me);
 	}
 	@RequestMapping(value = "/modMeMg", method = RequestMethod.POST)
-	public void modMeMg(Model model, @ModelAttribute Members me) {
+	public void modMeMg(Model model, @RequestBody Members me) {
 		this.me.backController("C15",me);
 	}
-	
-	
-}
