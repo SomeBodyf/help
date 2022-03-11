7996142a3422be1d531af141f7cb3521b1d98436
Bonnie <76897264+zmffhqj714@users.noreply.github.com>
2022-03-11 21:08:34 +0900

    Update ControllerYoung.java

--- src/main/java/com/somebody/controller/ControllerYoung.java
+++ src/main/java/com/somebody/controller/ControllerYoung.java
@@ -1,12 +1,7 @@
-package com.somebody.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.somebody.serviece.auth.Authenticaion;
 
@@ -14,29 +9,21 @@ import com.somebody.serviece.equimpment.Equipment;
 
 import com.somebody.serviece.lesson.Lesson;
 
-import com.somebody.serviece.member.Member;
-
 import com.somebody.serviece.pay.Pay;
-
-import com.somebody.serviece.staff.Staff;
 
 import beans.Equipments;
 import beans.Members;
 import beans.Pays;
-
-@Controller 
-public class ControllerYoung {
-	@Autowired
-	Authenticaion auth;
-	@Autowired
-	Equipment eq;
 	@Autowired
 	Pay pa;
 
-	@RequestMapping(value = "/goGoodsPage", method = RequestMethod.POST)
-	public void goGoodsPage(Model model, @ModelAttribute Equipments eq) {
-		this.eq.backController("G01",eq);
+	@RequestMapping("/goGoodsPage")
+	public ModelAndView goGoodsPage(Model model, @ModelAttribute Equipments eq) {
+		return this.eq.backController1("G01",eq);
 	}
+
+	@RequestMapping(value = "/payMg", method = RequestMethod.POST)
+	
 	
 	@RequestMapping(value = "/payMg", method = RequestMethod.POST)
 	public void payMg(Model model, @ModelAttribute Pays pa) {
