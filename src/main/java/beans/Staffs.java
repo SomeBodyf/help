951b403fda909d7674d4737644b46a1830b5860c
Bonnie <76897264+zmffhqj714@users.noreply.github.com>
2022-03-11 21:05:56 +0900

    Update Staffs.java

--- src/main/java/beans/Staffs.java
+++ src/main/java/beans/Staffs.java
@@ -7,16 +7,16 @@ import lombok.Data;
 @Data
 public class Staffs {
 	private String sfCtCode;
-	   private String sfId;
-	   private String sfName;
-	   private String sfPw;
-	   private String sfRank;
-	   private String sfNumber;
-	   private String sfEmail; 
-	   private String ahTime;
-	   private String ahIp;
-	   private int ahType;
-	   private String sfCaCode;
+	private String sfId;
+	private String sfName;
+	private String sfPw;
+	private String sfRank;
+	private String sfNumber;
+	private String sfEmail; 
+	private String ahTime;
+	private String ahIp;
+	private int ahType;
+	private String sfCaCode;
 	}
 
 	
