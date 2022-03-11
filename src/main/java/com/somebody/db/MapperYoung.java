d200d18dc06e1e4dc3caa66dde48666c7a0b9168
Bonnie <76897264+zmffhqj714@users.noreply.github.com>
2022-03-11 21:09:21 +0900

    Update MapperYoung.java

--- src/main/java/com/somebody/db/MapperYoung.java
+++ src/main/java/com/somebody/db/MapperYoung.java
@@ -4,4 +4,6 @@ import beans.Centers;
 
 public interface MapperYoung {
 	public int is(Centers ct);
+	public List<Equipments> goodsList(Equipments eq);
+	public List<Equipments> searchGoods(Equipments eq);
 }
