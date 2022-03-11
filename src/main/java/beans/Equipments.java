f66c4bbb6b2a62b715ba583733faa6546c175bb5
Bonnie <76897264+zmffhqj714@users.noreply.github.com>
2022-03-11 20:49:51 +0900

    Update Equipments.java

--- src/main/java/beans/Equipments.java
+++ src/main/java/beans/Equipments.java
@@ -4,14 +4,13 @@ import lombok.Data;
 
 @Data
 public class Equipments {
-	private String ctCode;
-	   private String eqCode;
-	   private String eqName;
-	   private String eqCaCode;
-	   private String eqCaName;
-	   private String egDate;
-	   private String sfCode;
-	   private String sfName;
-	   private String stCode;
-	   private String stName;
+	private String eqCode;
+	private String eqName;
+	private String eqCaCode;
+	private String eqCaName;
+	private String egDate;
+	private String sfCode;
+	private String sfName;
+	private String stCode;
+	private String stName;
 }
